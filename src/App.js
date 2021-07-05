0c1b4b4502c9fb99b17d5bb772dc1949bea21217
Pablo Bion <50814036+pablobion@users.noreply.github.com>
2021-07-05 00:00:05 -0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import ReactMarkdown from "react-markdown";
+import "react-markdown-editor-lite/lib/index.css";
 
+import MdEditor from "./components/mdEditor/index";
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <p>
+                    Edit <code>src/App.js</code> and save to reload.
+                </p>
+            </header>
+            <ReactMarkdown># Hello, *world*!</ReactMarkdown>
+            <MdEditor />
+        </div>
+    );
 }
 
 export default App;
